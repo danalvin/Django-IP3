a5c175fb6eb445bb2fee083c2f6d43e0266012eb
danalvin <mungaiwaituika@gmail.com>
2018-10-11 17:03:01 +0300

    templates added

--- awwards/urls.py
+++ awwards/urls.py
@@ -15,7 +15,15 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.conf.urls import url, include
+from django.contrib import admin
+from django.contrib.auth import views
+
+
+
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'', include('awards.urls')),
+
 ]
