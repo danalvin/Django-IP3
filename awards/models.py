eac39666823078954bb674a72c95787209bd9168
danalvin <mungaiwaituika@gmail.com>
2018-10-15 07:10:46 +0300

    added more views, adjusted the models and added the profile.html

--- awards/models.py
+++ awards/models.py
@@ -1,13 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
 
 # Create your models here.
 
-class User(models.Model):
-    name = models.CharField(max_length=20)
-    bio = models.TextField(max_length=200, blank=True)
-    email = models.CharField(max_length=100, null=True, blank=True)
 
-class Post(models.Model):
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE,related_name="profile")
+    bio = models.CharField(max_length=256)
+    profile_picture = models.ImageField(upload_to='profile_pictures/', blank=True, default="media/jVr43h8.png")
+
+    @receiver(post_save, sender=User)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
+
+    @receiver(post_save, sender=User)
+    def save_user_profile(sender, instance, **kwargs):
+        instance.profile.save()
+
+
+class Project(models.Model):
 
     image = models.ImageField(upload_to='images/')
     site_link= models.CharField(max_length=100)
@@ -17,3 +32,8 @@ class Post(models.Model):
 
     class Meta:
         ordering = ['-date']
+
+    @classmethod
+    def get_all(cls):
+        projects = cls.objects.all()
+        return projects
--- awards/views.py
+++ awards/views.py
@@ -1,9 +1,25 @@
 from django.shortcuts import render, redirect
+from .models import *
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+
 
 # Create your views here.
 def loader(request):
     return render(request, 'loader.html')
 
 
+@login_required(login_url='/accounts/login/')
 def index(request):
-    return render(request, 'index.html')
+    projects = Project.get_all()
+    return render(request, 'index.html', {'projects': projects})
+
+
+def post(request):
+    projects = Project.get_all()
+    return render(request, 'page.html', {'projects': projects})
+
+
+def profile(request, username):
+    user = User.objects.get(username=username)
+    return render(request, 'profile.html', {'user': user})
